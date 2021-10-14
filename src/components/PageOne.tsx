4230df278f6138ea676e778bb5639ed35c1f2465
bjorn <bjorn.thorlacius@knowit.no>
2021-10-14 21:01:22 +0200

    done

---
+++ src/components/PageOne.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function PageOne({}: Props): ReactElement {
+  return (
+    <div>
+      <h1>page 1</h1>
+    </div>
+  );
+}
---
+++ src/components/PageTwo.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function PageTwo({}: Props): ReactElement {
+  return (
+    <div>
+      <h1>page 2</h1>
+    </div>
+  );
+}
